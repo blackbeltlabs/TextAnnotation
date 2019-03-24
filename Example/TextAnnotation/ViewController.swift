a7f8495d295f1ecf1c12e35bdb90e4903b33e391
Sergey Vinogradov <wins.konar@gmail.com>
2019-03-24 16:57:20 +0200

    add responder protocol

--- Example/TextAnnotation/ViewController.swift
+++ Example/TextAnnotation/ViewController.swift
@@ -9,13 +9,47 @@
 import Cocoa
 import TextAnnotation
 
+protocol TAActivateResponder {
+    func textViewDidActivate(_ activeItem: Any?)
+}
 class TATextView: NSTextView {
     
+    // MARK: - Variables
+    
+    var activateResponder: TAActivateResponder?
+    
+    // MARK: - Methods
+    // MARK: Private
+    
+    override internal func mouseDown(with event: NSEvent) {
+        guard let responder = activateResponder else { return }
+        
+        responder.textViewDidActivate(nil)
+    }
 }
 
 class TAContainerView: NSView {
-    // MARK: - Variables
     
+    // MARK: - Variables
+//    var activateResponder: TAActivateResponder?
+    
+    var isActive: Bool = false {
+        didSet {
+            guard isActive != oldValue else { return }
+            
+            border.isHidden = !isActive
+            
+            guard textView != nil, let theWindow = textView.window else { return }
+            textView.backgroundColor = isActive ? NSColor.white : NSColor.clear
+            textView.textColor = isActive ? NSColor.black : NSColor.gray
+            
+            // FIXME: implement TAActivateResponder
+            if isActive, theWindow.firstResponder != textView {
+                theWindow.makeFirstResponder(textView)
+            }
+        }
+    }
+
     var text: String! {
         didSet {
             guard textView != nil else { return }
@@ -26,6 +60,7 @@ class TAContainerView: NSView {
     // MARK: Private
     
     private var textView: TATextView!
+    private var border: CALayer!
     
     // MARK: - Methods
     // MARK: Lifecycle
@@ -33,16 +68,26 @@ class TAContainerView: NSView {
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         
-        let kPadding: CGFloat = 10
+        let kPadding: CGFloat = 3
         let size = frameRect.size
 
         wantsLayer = true
-        layer?.backgroundColor = NSColor.red.cgColor
+//        layer?.backgroundColor = NSColor.red.cgColor
         
         textView = TATextView(frame: NSRect(origin: CGPoint(x: kPadding, y: kPadding),
                                                             size: CGSize(width: size.width - 2*kPadding, height: size.height - 2*kPadding)))
-        textView.backgroundColor = NSColor.blue
-        textView.textColor = NSColor.white
+        textView.backgroundColor = NSColor.clear
+        textView.textColor = NSColor.gray
+        textView.activateResponder = self
+        textView.textStorage?.delegate = self
+        
+        border = CALayer()
+        border.borderColor = NSColor.magenta.cgColor
+        border.borderWidth = 1
+        border.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        border.frame = CGRect(origin: CGPoint.zero, size: self.bounds.size)
+        border.isHidden = true
+        self.layer?.addSublayer(border)
         
         addSubview(textView)
     }
@@ -56,14 +101,35 @@ class TAContainerView: NSView {
     override internal func mouseDown(with event: NSEvent) {
         print("into annotation")
         
-        activate()
+        isActive = true
     }
     
     // MARK: - Public
     
-    func activate() {
-        guard textView != nil else { return }
-        textView.window?.makeFirstResponder(textView)
+//    func activate() {
+//        guard textView != nil, let theWindow = textView.window else { return }
+//        if theWindow.firstResponder != textView {
+//            theWindow.makeFirstResponder(textView)
+//        }
+//
+//        border.isHidden = false
+//    }
+//
+//    func inactivate() {
+//        border.isHidden = true
+//    }
+}
+
+extension TAContainerView: NSTextStorageDelegate {
+    func textStorage(_ textStorage: NSTextStorage, didProcessEditing editedMask: NSTextStorageEditActions, range editedRange: NSRange, changeInLength delta: Int) {
+        
+        
+    }
+}
+
+extension TAContainerView: TAActivateResponder {
+    func textViewDidActivate(_ activeItem: Any?) {
+        isActive = true
     }
 }
 
@@ -88,11 +154,13 @@ class ViewController: NSViewController, TextAnnotationsController {
         
         let view1 = TAContainerView(frame: NSRect(origin: location, size: size))
         view1.text = "ann. 1"
+//        view1.activateResponder = self
         view.addSubview(view1)
         annotations.append(view1)
         
         let view2 = TAContainerView(frame: NSRect(origin: CGPoint(x: 50, y: 20), size: size))
         view2.text = "ann. 2"
+//        view2.activateResponder = self
         view.addSubview(view2)
         annotations.append(view2)
         
@@ -103,9 +171,9 @@ class ViewController: NSViewController, TextAnnotationsController {
   override func mouseDown(with event: NSEvent) {
     // TextAnnotationsController needs to handle mouse down events
 //    textAnnotationsMouseDown(event: event)
-//    for item in annotations {
-//        item.inactivate()
-//    }
+    for item in annotations {
+        item.isActive = false
+    }
     view.window?.makeFirstResponder(nil)
   }
   
@@ -113,6 +181,10 @@ class ViewController: NSViewController, TextAnnotationsController {
     // TextAnnotationsController needs to handle drag events
     textAnnotationsMouseDragged(event: event)
   }
+    
+    // MARK: - Private
+    
+    
 }
 
 extension ViewController: TextAnnotationDelegate {
@@ -124,3 +196,9 @@ extension ViewController: TextAnnotationDelegate {
     print(textAnnotation.frame)
   }
 }
+
+//extension ViewController: TAActivateResponder {
+//    func textViewDidActivate() {
+//        
+//    }
+//}
