eb0d6201a445cc92798001404cb20f64cb8fc2b6
Mirko Kiefer <mail@mirkokiefer.com>
2019-05-20 22:25:14 +0700

    Refactor to comply with spec

--- TextAnnotation/Classes/TextAnnotation.swift
+++ TextAnnotation/Classes/TextAnnotation.swift
@@ -1,6 +1,11 @@
-import Foundation
+import Cocoa
 
-public protocol TextAnnotation {
+public protocol TextAnnotation where Self: NSView {
   var text: String { get set }
   var frame: CGRect { get set }
+  var state: TextAnnotationState { get set }
+}
+
+extension TextContainerView: TextAnnotation {
+  
 }
