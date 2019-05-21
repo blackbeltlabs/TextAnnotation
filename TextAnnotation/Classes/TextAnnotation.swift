b278c6ccd9f383ff01d40d563aab0ac46f6a78c1
Mirko Kiefer <mail@mirkokiefer.com>
2019-05-22 00:28:47 +0700

    Add select/deselect interface

--- TextAnnotation/Classes/TextAnnotation.swift
+++ TextAnnotation/Classes/TextAnnotation.swift
@@ -4,12 +4,20 @@ public protocol TextAnnotation where Self: NSView {
   var delegate: TextAnnotationDelegate? { get set }
   var text: String { get set }
   var frame: CGRect { get set }
-  var state: TextAnnotationState { get set }
+  var state: TextAnnotationState { get set }  
 }
 
 extension TextAnnotation {
   public func delete() {
     removeFromSuperview()
+  }
+  
+  public func select() {
+    state = .active
+  }
+  
+  public func deselect() {
+    state = .inactive
   }
 }
 
