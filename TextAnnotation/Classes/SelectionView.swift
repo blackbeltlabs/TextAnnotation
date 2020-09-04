7b476d5978268f29a3a9b267265020b52d096ded
Julian Drapailo <ulian_onua@ukr.net>
2020-09-04 17:10:06 +0300

    Update cursor to move text annotation

--- TextAnnotation/Classes/SelectionView.swift
+++ TextAnnotation/Classes/SelectionView.swift
@@ -3,14 +3,23 @@ import Cocoa
 @IBDesignable
 class SelectionView: NSView {
   
+  var currentSelectionFrame: NSRect {
+    selectionFrame(with: frame)
+  }
+  
+  func selectionFrame(with frameRect: NSRect) -> NSRect {
+    let padding = Configuration.frameMargin + Configuration.dotRadius
+    return NSRect(x: padding,
+                  y: padding,
+                  width: frameRect.width - 2 * padding,
+                  height: frameRect.height - 2 * padding)
+  }
+  
   override func draw(_ dirtyRect: NSRect) {
     super.draw(dirtyRect)
     
     let padding = Configuration.frameMargin + Configuration.dotRadius
-    let framePath = NSBezierPath(rect: NSRect(x: padding,
-                                              y: padding,
-                                              width: dirtyRect.width - 2 * padding,
-                                              height: dirtyRect.height - 2 * padding))
+    let framePath = NSBezierPath(rect: selectionFrame(with: dirtyRect))
     
     framePath.lineWidth = Configuration.controlStrokeWidth
     Palette.frameStrokeColor.set()
